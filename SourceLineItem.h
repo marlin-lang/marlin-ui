3d4a49db17797c96c25f4aff34c0e48b0ec25eb5
codingpotato <codingpotato@gmail.com>
2019-07-27 22:28:28 +0800

    Show mar source code

---
+++ SourceLineItem.h
@@ -0,0 +1,16 @@
+//
+//  SourceLineItem.h
+//  marlin
+//
+//  Created by Wang Yongwu on 2019/7/27.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SourceLineItem : NSCollectionViewItem
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SourceLineItem.m
@@ -0,0 +1,21 @@
+//
+//  SourceLineItem.m
+//  marlin
+//
+//  Created by Wang Yongwu on 2019/7/27.
+//
+
+#import "SourceLineItem.h"
+
+@interface SourceLineItem ()
+
+@end
+
+@implementation SourceLineItem
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
